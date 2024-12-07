8172ba77390cb76178c9a3730804c0fc072e8ae6
Андрей <Barbus1966@gmail.com>
2024-12-07 13:40:16 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+#Динамическая типизация
+name = 'Андрей'
+print('Имя:', name)
+age = 58
+print('Возраст:',age)
+age = age + 2
+print('Новый возраст:',age)
+is_student = True
+print(is_student)
+
+
+
